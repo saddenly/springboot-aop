2f44d4291b0556a91ba83c68a77dab3da579144c
saddenly <rustemandasov@outlook.com>
2024-04-04 15:44:06 +0200

    initial commit, beforeAdvice implemented

---
+++ src/main/java/com/example/aopdemo/AopdemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.aopdemo;
+
+import com.example.aopdemo.dao.AccountDAO;
+import com.example.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopdemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+        return runner -> {
+            demoBeforeAdvice(accountDAO, membershipDAO);
+        };
+    }
+
+    private void demoBeforeAdvice(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+        accountDAO.addAccount();
+        System.out.println();
+        membershipDAO.addAccount();
+    }
+}
---
+++ src/main/java/com/example/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,5 @@
+package com.example.aopdemo.dao;
+
+public interface MembershipDAO {
+    void addAccount();
+}
